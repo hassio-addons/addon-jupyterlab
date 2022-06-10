63dcb81b1f5a9cf48bc56102a326308cbbda11a2
Franck Nijhof <git@frenck.dev>
2022-06-10 08:25:32 +0200

    ⬆️ Upgrades add-on base image to 6.0.0 (#312)

--- jupyterlab/rootfs/etc/cont-init.d/notebooks.sh
+++ jupyterlab/rootfs/etc/cont-init.d/notebooks.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/with-contenv bashio
+#!/command/with-contenv bashio
 # ==============================================================================
 # Home Assistant Community Add-on: JupyterLab
 # Ensures the JupyterLab notebooks directory exists
